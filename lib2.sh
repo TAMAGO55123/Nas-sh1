ee699156b324c7c4991c80350bd4515eb49fcd0c
TAMAGO55123 <147392915+TAMAGO55123@users.noreply.github.com>
2024-05-26 15:57:42 +0900

    update

--- lib2.sh
+++ lib2.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
-mkdir lib
-cd lib
-wget https://www.python.org/ftp/python/3.12.3/Python-3.12.3.tgz
-tar -xf Python-3.12.3.tgz
-cd Python-3.12.3
-./configure --enable-optimizations
-make -j 12
+sudo apt install manpages-ja manpages-ja-dev -y
+
+sudo apt install python3 -y
 
 sudo apt install apache2 -y
 sudo ufw app list
 sudo ufw allow 'Apache'
+sudo ufw allow 22
 sudo ufw enable
 hostname -I
 sudo apt install php libapache2-mod-php -y
